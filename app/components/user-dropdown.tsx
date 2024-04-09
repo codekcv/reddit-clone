e78db2540d5f8def682438e33ec1dbe9a0c786c2
codekcv <ChristianVillamin31@gmail.com>
2024-04-09 23:08:07 +0800

    feat: dropdown options and logout kinde

--- app/components/user-dropdown.tsx
+++ app/components/user-dropdown.tsx
@@ -1,9 +1,14 @@
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export function UserDropdown() {
   return (
@@ -11,8 +16,41 @@ export function UserDropdown() {
       <DropdownMenuTrigger>
         <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            className="rounded-full w-8 h-8 hidden lg:block"
+            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+            alt="user avatar"
+          />
         </div>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-[200px]" align="end">
+        <DropdownMenuItem>
+          <Link className="w-full" href="/r/create">
+            Create Community
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Link className="w-full" href="/create">
+            Create Post
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Link className="w-full" href="/settings">
+            Settings
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <LogoutLink className="w-full">Logout</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
